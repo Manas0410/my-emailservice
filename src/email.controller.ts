e9197ec5489ce1da0d0b3850aaf352f9512ee9b4
Manas0410 <manasshrivastava0410@gmail.com>
2024-09-26 01:13:35 +0530

    etra info

--- src/email.controller.ts
+++ src/email.controller.ts
@@ -5,15 +5,27 @@ import { MailOptions, sendMail } from "../utils/mailConfigurations";
 const mailController = async (req: Request, res: Response) => {
   try {
     const { tomail, bcc = "", subject, message, information } = req.body;
-    console.log(information);
+
+    // Serialize the information object (convert it to a string if necessary)
+    const informationString = information
+      ? JSON.stringify(information, null, 2)
+      : "";
+
+    // Append the serialized information to the message
+    const fullMessage = `
+      ${message}
+      
+      Additional Information:
+      ${informationString}
+    `;
 
     const mailOptions: MailOptions = {
       from: `Team MANAS <${senderMailAddress}>`,
       to: tomail,
       bcc: bcc,
       subject: subject,
-      text: message,
-      information,
+      text: fullMessage,
+      information: "",
     };
 
     await sendMail(mailOptions);
